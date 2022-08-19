378e1061e3128f4e8cf8d9763ec14697929c1e1c
Shavkatbek <shavkatbekmaxammadiyev@gmail.com>
2022-08-19 23:02:38 +0500

    loop project start

--- js/database.js
+++ js/database.js
@@ -1,11 +1,20 @@
-const users = [
+let users = window.localStorage.getItem('users')
+let foods = window.localStorage.getItem('foods')
+let orders = window.localStorage.getItem('orders')
+
+
+
+
+
+
+ users = JSON.parse(users) || [
     { userId: 1, username: 'Ali', contact: '9989110032505' },
     { userId: 2, username: 'Vali', contact: '99891100552' },
     { userId: 3, username: 'Laziza', contact: '998911002245' },
     { userId: 4, username: 'Alisher', contact: '998911005741' }
 ]
 
-const foods = [
+foods = JSON.parse(foods) || [
     { foodId: 1, foodname: 'burger cheese', foodImg: './img/burger_cheese.jpeg' },
     { foodId: 2, foodname: 'chicken togora', foodImg: './img/chicken_togora.jpeg' },
     { foodId: 3, foodname: 'chicken wings', foodImg: './img/chicken_wings.jpeg' },
@@ -15,7 +24,7 @@ const foods = [
     { foodId: 6, foodname: 'spinner', foodImg: './img/spinner.jpeg' },
 ]
 
-const orders = [
+orders = JSON.parse(orders) || [
     { userId: 1, foodId: 1, count: 2 },  // ali 2 ta burger
     { userId: 3, foodId: 6, count: 1 },  // laziza 1 ta spinder
     { userId: 2, foodId: 4, count: 3 }   // vali 3 ta cola
--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ const foodsForm = document.querySelector('#foodsForm')
 const foodsCount = document.querySelector('#foodsCount')
 
 
-function renderUsers () {
+function renderUsers() {
     customersList.innerHTML = null
-    for(let user of users){
-        const [li, span, a] = createElement ('li','span', 'a')
+    for (let user of users) {
+        const [li, span, a] = createElement('li', 'span', 'a')
         li.classList.add('customer-item')
         span.classList.add('customer-name')
         a.classList.add('customer-phone')
@@ -22,99 +22,142 @@ function renderUsers () {
         span.textContent = user.username
         a.textContent = user.contact
 
-        li.append(span,a)
+        li.append(span, a)
         customersList.append(li)
 
         li.addEventListener('click', () => {
             renderOrders(user.userId)
             clientId.textContent = user.userId
             customerName.textContent = user.username
-            // console.log('hello');
+
+            window.localStorage.setItem('userId', user.userId)
+            window.localStorage.setItem('username', user.username)
         })
     }
 
 }
 
-function renderFoods () {
-    for(let food of foods){
+function renderFoods() {
+    for (let food of foods) {
         const [option] = createElement('option')
         option.value = food.foodId
         option.textContent = food.foodname
         foodsSelect.append(option)
     }
-} 
+}
 
-function renderOrders (userId) {
+function renderOrders(userId) {
+    if (!userId) return
     ordersList.innerHTML = null
-    for(let order of orders){
-        if(!(order.userId == userId)) continue
+    for (let order of orders) {
+        if (!(order.userId == userId)) continue
         const food = foods.find(el => el.foodId == order.foodId);
 
-        const [liEl,imgEl,divEl,nameEl,countEl] = createElement('li','img', 'div', 'span', 'span')
+        const [liEl, imgEl, divEl, nameEl, countEl] = createElement('li', 'img', 'div', 'span', 'span')
         liEl.classList.add('order-item')
         nameEl.classList.add('order-name')
         countEl.classList.add('order-count')
         imgEl.setAttribute('src', food.foodImg)
-        nameEl.textContent = food.foodname        
+        nameEl.textContent = food.foodname
         countEl.textContent = order.count
-        divEl.append(nameEl,countEl)
-        liEl.append(imgEl,divEl)
+        divEl.append(nameEl, countEl)
+        liEl.append(imgEl, divEl)
         ordersList.append(liEl)
 
     }
-} 
+}
 
-function addUser () {
+function addUser(event) {
+    event.preventDefault()
 
-} 
-function addOrder (event) {
+    const username = usernameInput.value.trim()
+    const contact = telephoneInput.value.trim()
+
+
+    if (!username || username.length > 30) {
+        return alert('Invalid Username')
+    }
+
+    if ((/998(9[0123456789]|3[3]|7[1]|8[8])[0-9]{7}$/).test(contact)) {
+        return alert('Invalid Phone')
+    }
+
+    const newUser = {
+        userId: users.length ? users[users.length - 1].userId + 1 : 1,
+        username: username,
+        contact: contact
+    }
+    users.push(newUser)
+    window.localStorage.setItem('users', JSON.stringify(users))
+    return renderUsers()
+}
+
+function addOrder(event) {
     event.preventDefault()
 
     const foodId = foodsSelect.value.trim()
     const count = foodsCount.value.trim()
     const userId = clientId.textContent.trim()
 
-    if(
+    // const foodId = foodsSelect.value.trim()
+    // const count = foodsCount.value.trim()
+    // const userId = clientId.textContent.trim()
+
+    let order = orders.find(el => el.foodId == foodId && el.userId)
+
+    if (
         !count ||
         +count > 10 ||
         !userId
     ) return
 
-    const newOrder = {
-        foodId,
-        userId,
-        count
+    if (order) {
+        order.count = +count + +order.count
+    } else {
+        order = { foodId, userId, count }
+        orders.push(order)
     }
-    orders.push(newOrder)
+
+    window.localStorage.setItem('orders', JSON.stringify(orders))
     return renderOrders(userId)
 
 }
 
-userAdd.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    const username = usernameInput.value.trim()
-    const contact = telephoneInput.value.trim()
-
-    if(!username || username.length > 30){
-        return alert('Invalid Username')
-    }
-
-    if((/998(9[0123456789]|3[3]|7[1]|8[8])[0-9]{7}$/).test(contact)){
-        return alert('Invalid Phone')
-    }
-
-    const newUser = {
-        userId: users.length ? users[users.length -1].userId + 1 : 1,
-        username: username,
-        contact: contact
-    }
-    users.push(newUser)
-    return renderUsers()
-
-})
+userAdd.addEventListener('submit', addUser)
 foodsForm.addEventListener('submit', addOrder)
 
+// userAdd.addEventListener('submit', (event) => {
+// event.preventDefault()
+
+// const username = usernameInput.value.trim()
+// const contact = telephoneInput.value.trim()
+
+// if(!username || username.length > 30){
+//     return alert('Invalid Username')
+// }
+
+// if((/998(9[0123456789]|3[3]|7[1]|8[8])[0-9]{7}$/).test(contact)){
+//     return alert('Invalid Phone')
+// }
+
+// const newUser = {
+//     userId: users.length ? users[users.length -1].userId + 1 : 1,
+//     username: username,
+//     contact: contact
+// }
+// users.push(newUser)
+// window.localStorage.setItem('users', JSON.stringify(users))
+// return renderUsers()
+
+// })
+// foodsForm.addEventListener('submit', addOrder)
+
+const userId = window.localStorage.getItem('userId')
+const username = window.localStorage.getItem('username')
+
+username && (customerName.textContent = username)
+userId && (clientId.textContent = userId)
 // CRUD => create read update delete
 renderUsers()
 renderFoods()
+renderOrders(userId)
